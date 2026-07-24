3b7d7b36a984e051a1c4693fd5d704bc60ed46d3
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2026-07-24 18:56:53 -0300

    Update player script

--- BaseGame/scripts/player.gd
+++ BaseGame/scripts/player.gd
@@ -1,30 +1,33 @@
 extends CharacterBody2D
 
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
+@onready var revolver_sprite: Sprite2D = $RevolverSprite
 
+var revolverPosition: float 
 const SPEED = 150.0
-# const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	revolverPosition = revolver_sprite.position.x
+
+func _process(delta: float) -> void:
+	
+	
+	var mousePosition = get_global_mouse_position()
+	
+	if global_position.x < mousePosition.x:
+		revolver_sprite.flip_v = false
+		revolver_sprite.position.x = revolverPosition
+		player_sprite.flip_h = false
+		
+	else:
+		revolver_sprite.position.x = - revolverPosition
+		revolver_sprite.flip_v = true
+		player_sprite.flip_h = true
+		
+	
+	revolver_sprite.look_at(mousePosition)
 
 func _physics_process(delta: float) -> void:
-	
-	"""
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	"""
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
 
