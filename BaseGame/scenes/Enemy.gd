b4da98410718cb457af97d78b7f71d91d7a68b52
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2026-07-26 12:07:30 -0300

    Create simple enemy

---
+++ BaseGame/scenes/Enemy.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var alpha_zero_color : Color
+
+func _ready() -> void:
+	area_entered.connect(area_entered_func)
+
+func area_entered_func(area : Area2D):
+	die()
+
+func die():
+	sprite.play("Die")
+	await sprite.animation_finished
+	var tween = create_tween()
+	tween.tween_property(sprite, "self_modulate", alpha_zero_color, 0.2)
+	await tween.finished
+	queue_free()
